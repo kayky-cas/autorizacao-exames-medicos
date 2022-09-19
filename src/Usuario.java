015c613ef8e091348be1cf687460dfde7e401cd1
Kayky Casagrande <kayky-cas@github.com>
2022-09-19 16:35:35 -0300

    implement: id generator in Usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,4 +1,11 @@
 public abstract class Usuario {
+    private static int actualId = 0;
+    
     private int id;
     private String nome;
+
+    public Usuario(String nome) {
+        id = actualId++;
+        this.nome = nome;
+    }
 }
