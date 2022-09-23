c22a3090f8fec999bc588df1364ffa9bf6c3c891
Gabriel Medeiros <gabriel.medeiros1903@outlook.com>
2022-09-23 18:10:34 -0300

    atributos paciente

--- src/models/Paciente.java
+++ src/models/Paciente.java
@@ -4,10 +4,16 @@ import models.Autorizacao;
 
 import java.util.ArrayList;
 
-public class Paciente {
+
+public class Paciente extends Usuario {
 
 	private String cpf;
 
 	private ArrayList<Autorizacao> autorizacoes;
 
+	public Paciente(String cpf, String nome){
+		super(nome);
+		this.cpf = cpf;
+		autorizacoes = new ArrayList<>();
+	}
 }
