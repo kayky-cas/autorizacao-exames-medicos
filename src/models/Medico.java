bc975b2f781a2baa0dfbd6fbb69d838ad4a399cd
Andressa Farkas <andressafarkas08@gmail.com>
2022-09-28 10:29:54 -0300

    added constructor method

--- src/models/Medico.java
+++ src/models/Medico.java
@@ -1,14 +1,39 @@
 package models;
 
 import enums.Especializacao;
+import enums.TipoExames;
 import models.Autorizacao;
 
 import java.util.ArrayList;
 
 public class Medico {
 
-	private Especializacao especificacao;
+	private int id;
+	private String nome;
+	private double salario;
+	private boolean estacionamento;
+	private Especializacao especializacao;
 
 	private ArrayList<Autorizacao> autorizacoes;
+
+	public Medico(int id, String nome, double salario, boolean estacionamento, Especializacao especializacao){
+		this.id = id;
+		this.nome = nome;
+		this.salario = salario;
+		this.estacionamento = estacionamento;
+		this.especializacao = especializacao;
+	}
+
+	public boolean addAutorizacao(Autorizacao autorizacao){
+		return true;
+	}
+
+	public Autorizacao[] listaAutorizacaoPaciente(String cpf){
+		return null;
+	}
+	
+	public Autorizacao[] listaAutorizacaoTipo(TipoExames tipo){
+		return null;
+	}
 
 }
