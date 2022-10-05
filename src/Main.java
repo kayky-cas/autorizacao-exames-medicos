c5e8dbb780ad367d5052ee4083a815ea4dac3ad8
VitorVenturini <vfventurini@gmail.com>
2022-10-05 19:59:43 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import libs.ACMEHospital;
+import models.*;
 public class Main {
     //Estou fazendo a main: Vitor Venturini
     //Criei minha SHH
+    public static void main(String[] args) {
+        ACMEHospital H = new ACMEHospital();
+        H.executa();    
+    }
 
 }
