3aca9e9df86c358b1dca7fdb1f1b76b1a7e2fbb1
Giancarlo Mena <giancarlo.mena@outlook.com>
2022-10-06 19:08:09 -0300

    implemented especializacao enum class

--- src/enums/Especializacao.java
+++ src/enums/Especializacao.java
@@ -2,4 +2,20 @@ package enums;
 
 public enum Especializacao {
 
+    CARDIOLOGISTA("cardiologista"),
+    ACUPUNTURA("acupuntura"),
+    CIRURGIAO_PLASTICO("cirurgiao plastico"),
+    OTORRINOLARINGOLOGISTA("otorrinolaringologista"),
+    OFTALMOLOGISTA("oftalmologista"),
+    GINECOLOGISTA("ginecologista");
+
+    private String nome;
+
+    private Especializacao(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
