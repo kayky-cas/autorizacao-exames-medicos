98a913fd1fb0cf226562966cd77f5d804bbd92cd
Giancarlo Mena <giancarlo.mena@outlook.com>
2022-10-06 17:46:57 -0300

    fixed medico and funcionario

--- src/models/Funcionario.java
+++ src/models/Funcionario.java
@@ -1,9 +1,15 @@
 package models;
 
-public abstract class Funcionario {
+public abstract class Funcionario extends Usuario{
 
 	private double salario;
 
 	private boolean estacionamento;
+
+	public Funcionario(String nome, double salario) {
+		super(nome);
+		this.salario = salario;
+		estacionamento = true;
+	}
 
 }
