d40d983d0fee52882da8ceec515ca0a8e77c5225
Kayky Casagrande <kayky-cas@github.com>
2022-10-06 17:21:54 -0300

    Update: Construtor SistemaInterno

--- src/libs/SistemaInterno.java
+++ src/libs/SistemaInterno.java
@@ -2,16 +2,19 @@ package libs;
 
 import models.Administrador;
 import models.Autorizacao;
+import models.Paciente;
 import models.Usuario;
 
 import java.util.ArrayList;
 
 public class SistemaInterno {
-
 	private ArrayList<Usuario> usuarios;
-
-	private Administrador adm;
-
+	private Administrador administrador;
 	private ArrayList<Autorizacao> autorizacoes;
 
+	public SistemaInterno() {
+		this.usuarios = new ArrayList<>();
+		this.administrador = null;
+		this.autorizacoes = new ArrayList<>();
+	}
 }
