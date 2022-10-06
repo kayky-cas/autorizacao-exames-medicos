0edc94fa01ee5e5b140c59761d700a1c68be2eec
Thom-Mundstock <thomasmundstock@gmail.com>
2022-10-06 13:25:12 -0300

    getters

--- src/models/Autorizacao.java
+++ src/models/Autorizacao.java
@@ -8,7 +8,7 @@ public class Autorizacao {
 
 	private int codigo;
 
-	private Date data;
+	private Date data_cadastro;
 
 	private Medico medico;
 
@@ -16,12 +16,32 @@ public class Autorizacao {
 
 	private Exame exame;
 
-	public Autorizacao(int codigo, Date data, Medico medico, Paciente paciente, Exame exame){
+	public Autorizacao(int codigo, Date data_cadastro, Medico medico, Paciente paciente, Exame exame){
 		this.codigo = codigo;
-		this.data = data;
+		this.data_cadastro = data_cadastro;
 		this.medico = medico;
 		this.paciente = paciente;
 		this.exame = exame;
+	}
+
+	public int get_codigo(){
+		return codigo;
+	}
+
+	public Date get_data_cadastro(){
+		return data_cadastro;
+	}
+
+	public Medico get_medico(){
+		return medico;
+	}
+
+	public Paciente get_paciente(){
+		return paciente;
+	}
+
+	public Exame get_exame(){
+		return exame;
 	}
 
 }
