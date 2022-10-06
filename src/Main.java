d7235c7814fcf8878bd395a6deaaf5694fdfc190
Kayky Casagrande <kayky-cas@github.com>
2022-10-06 17:02:48 -0300

    Update: ACMEHospital variable

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import libs.ACMEHospital;
 import models.*;
 public class Main {
-    //Estou fazendo a main: Vitor Venturini
-    //Criei minha SHH
     public static void main(String[] args) {
-        ACMEHospital H = new ACMEHospital();
-        H.executa();    
+        ACMEHospital hospital = new ACMEHospital();
+        hospital.inicializa();
+        hospital.executa();
     }
 
 }
