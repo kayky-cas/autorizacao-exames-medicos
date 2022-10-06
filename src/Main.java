0cc7bd1a5759c6738e1e6103dc22f28a41b29e75
Giancarlo Mena <giancarlo.mena@outlook.com>
2022-10-05 22:18:53 -0300

    Revert "Vitor/main"

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,3 @@
-import libs.ACMEHospital;
-import models.*;
 public class Main {
-    //Estou fazendo a main: Vitor Venturini
-    //Criei minha SHH
-    public static void main(String[] args) {
-        ACMEHospital H = new ACMEHospital();
-        H.executa();    
-    }
 
 }
